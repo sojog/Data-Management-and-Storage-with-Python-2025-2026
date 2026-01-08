ce34f9eff06dbde9061e345ffd6d3a8a59347803
Silviu Ojog <sil.ojog@gmail.com>
2026-01-08 19:13:50 +0200

    Lotto -  6 numere

---
+++ Curs1/04.Lotto.py
@@ -0,0 +1,40 @@
+import random
+
+class Lotto:
+    def __init__(self, minim, maxim):
+        self.min = minim
+        self.max = maxim
+
+    def genereaza_numar(self):
+        return random.randint(self.min, self.max)
+    
+    def genereaza_6_numere(self):
+        numere = []
+        while len(numere) < 6:
+            nr_nou = self.genereaza_numar()
+            if nr_nou not in numere:
+                numere.append(nr_nou)
+        return numere
+
+
+extragere = Lotto(1, 49)
+numere = extragere.genereaza_6_numere()
+print(numere)
+
+numere = [ f"{i}\n" for i in numere ]
+print(numere)
+
+with open("04.numere_extrase_lista.txt", "a") as file_writer:
+    file_writer.writelines(numere)
+
+
+
+# for i in range(6):
+#     nr = extragere.genereaza_numar()
+#     print(nr)
+#     with open("04.numere_extrase.txt", "a") as file_writer:
+#         file_writer.write(str(nr) + "\n")
+
+
+
+
